ce6cdad3f4f144065a8d1aed8d6a20144aa27c15
Devpanditji <92287822+Devpanditji@users.noreply.github.com>
2021-12-12 02:25:53 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,13 +52,13 @@ function draw() {
       /*Uncomment correct option 
         according to PLAY state*/  
       // // moving ground
-      // scene.velocityX = -3 
+       scene.velocityX = -3 
       // //destroy bow
       // bow.destroy();
       // //reset the background
-      // if (scene.x < 0){
-        //   scene.x = scene.width/2;
-         // }
+       if (scene.x < 0){
+           scene.x = scene.width/2;
+          }
       // //moving bow
        bow.y = World.mouseY      
       //stop background movement
@@ -103,7 +103,7 @@ function draw() {
       // //moving bow
       // bow.y = World.mouseY      
       // //stop background movement
-      // scene.velocityX = 0;
+       scene.velocityX = 0;
 
   }
 
